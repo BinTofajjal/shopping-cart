83533a54a5dde77ed4da45c3c6aa264a3a65dae0
BinTofajjal <HabibBinTofajjal@icloud.com>
2022-10-02 08:24:01 +0600

    price values adjusted

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-function productUpdate(product, increasing) {
+function productUpdate(product, increasing, price, totalPrice) {
   const productInput = document.getElementById(product);
   let productNumber = parseInt(productInput.value);
   if (increasing) {
-    productInput.value = productNumber + 1;
+    productNumber = productNumber + 1;
   } else if (productNumber > 0) {
-    productInput.value = productNumber - 1;
+    productNumber = productNumber - 1;
   } else {
     alert('You have nothing to remove');
   }
+  productInput.value = productNumber;
+  // Handle Input Value Price
+  const priceAmount = document.getElementById(totalPrice);
+  const priceValue = parseInt(priceAmount.innerText);
+  priceAmount.innerText = productNumber * price;
 }
 
-/* Handle Phone */
+/* Handle Phones */
 // Adding phones
 document.getElementById('phone-plus').addEventListener('click', function () {
-  productUpdate('phone-number', true);
+  productUpdate('phone-number', true, 1219, 'phone-price');
 });
 // Removing Phones
 document.getElementById('phone-minus').addEventListener('click', function () {
-  productUpdate('phone-number', false);
+  productUpdate('phone-number', false, 1219, 'phone-price');
 });
 
+/* Handle cases */
 // Adding cases
 document.getElementById('case-plus').addEventListener('click', function () {
-  productUpdate('case-number', true);
+  productUpdate('case-number', true, 59, 'case-price');
 });
 // Removing cases
 document.getElementById('case-minus').addEventListener('click', function () {
-  productUpdate('case-number', false);
+  productUpdate('case-number', false, 59, 'case-price');
 });
